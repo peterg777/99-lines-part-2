e6eddb38b530a8efe3c32f2a3faebf99f25182a9
peterg777 <ptrg777@gmail.com>
2020-02-06 20:01:39 -0500

    adding js and html btn

---
+++ app.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', button);
+function() {
+    let button = document.createElement('button');
+    let btnText = document.createTextNode('sing');
+    button.appendChild(btnText);
+    document.body.appendChild(button);
+    let friends[i] = ['rich', 'marty', 'brian', 'christy', 'lena'];
+}
+
+for (let i = 0; i < friends.length; i++) {
+    let h3 = document.createElement('h3')
+    let h3Text = document.createTextNode(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file ");
+    h3.appendChild(h3Text);
+    document.body.appendChild(h3);
+    let div = document.createElement('div')
+    div.className = friend;
+}
+
+for (j = 99; j > 0; j--) {
+    document.createElement()
+
+}
+
