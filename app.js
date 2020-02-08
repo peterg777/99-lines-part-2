1ff46e5ea5874f211798e4fa46994011b7520cec
peterg777 <ptrg777@gmail.com>
2020-02-07 20:39:02 -0500

    adding js

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
-document.addEventListener('DOMContentLoaded', button);
-function() {
-    let button = document.createElement('button');
-    let btnText = document.createTextNode('sing');
-    button.appendChild(btnText);
-    document.body.appendChild(button);
-    let friends[i] = ['rich', 'marty', 'brian', 'christy', 'lena'];
+let friends = ['rich', 'marty', 'brian', 'christy', 'lena'];
+
+let button = document.createElement('button');
+let btnText = document.createTextNode('sing');
+button.appendChild(btnText);
+document.body.appendChild(button);
+
+button.className = 'btn btn-warning';
+button.addEventListener('click', function () {
+    for (let i = 0; i < friends.length; i++) {
+        let h3 = document.createElement('h3');
+        let h3Text = document.createTextNode(friends[i]);
+        h3.appendChild(h3Text);
+        let div = document.createElement('div');
+        div.className = 'friend';
+        div.appendChild(h3);
+        document, body.appendChild(h3);
+
+
+        for (let j = 99; j > 0; j--) {
+
+            let p = document.createElement('p');
+            let pText;
+
+            if (j > 2) {
+                pText = document.createTextNode(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file");
+            } else if (j === 2) {
+                pText = document.createTextNode(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file");
+            } else {
+                pText = document.createTextNode(j + " line of code in the file, " + j + " line of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
+            }
+
+            p.appendChild(pText);
+
+            div.appendChild(p);
+        }
+
+
+        document.body.appendChild(div);
+    }
 }
-
-for (let i = 0; i < friends.length; i++) {
-    let h3 = document.createElement('h3')
-    let h3Text = document.createTextNode(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file ");
-    h3.appendChild(h3Text);
-    document.body.appendChild(h3);
-    let div = document.createElement('div')
-    div.className = friend;
-}
-
-for (j = 99; j > 0; j--) {
-    document.createElement()
-
-}
-
